53bf0426072e19c4a371c2db344b7bdee18d4939
Mike Levine <mike.levine@gmail.com>
2015-10-28 13:23:22 -0400

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  authenticated :user do
+  	root 'home#index', as: :authenticated_root
+  end
+
+  devise_scope :user do
+  	root :to => 'devise/sessions#new'
+  end
+  
   devise_for :users
   resources :users, :only => [:index]
 end
