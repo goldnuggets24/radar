70831531066a6bbdf7d7bcf0bed446489fc44125
Mike Levine <mike.levine@gmail.com>
2015-11-20 11:40:58 -0500

    adding tables

--- app/assets/javascripts/components/users/profile.jsx
+++ app/assets/javascripts/components/users/profile.jsx
@@ -11,18 +11,24 @@ var FlatButton = require('material-ui/lib/flat-button');
 var Checkbox = require('material-ui/lib/checkbox');
 var DatePicker = require('../events_calendar/date-picker');
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+var Table = require('material-ui/lib/table/table');
+var TableBody = require('material-ui/lib/table/table-body');
+var TableFooter = require('material-ui/lib/table/table-footer');
+var TableHeader = require('material-ui/lib/table/table-header');
+var TableHeaderColumn = require('material-ui/lib/table/table-header-column');
+var TableRow = require('material-ui/lib/table/table-row');
+var TableRowColumn = require('material-ui/lib/table/table-row-column');
 
 module.exports = React.createClass({
   displayName: 'Profile',
 
   handleSubmit: function(i, role) {
-    debugger;
     // uncheck / check based on selection
     var selected_user = ({});
     if (this.props.users.map(function(user){ if (user.id == i) {selected_user = user} }));
     var myArray = selected_user.events.map(function(i) {return i.id});
 
-    if (this.refs["team_lead_checkbox-"+i].state.switched == false) {
+    if (this.refs[role+"_checkbox-"+i].state.switched == false) {
 
       // if ($.inArray(this.props.selectedEvent, myArray) == -1) {
         $.get('/events/' + this.props.selectedEvent + '/add_user', {
@@ -41,6 +47,18 @@ module.exports = React.createClass({
   },
 
   render: function() {
+    this.state = {
+      fixedHeader: true,
+      fixedFooter: true,
+      stripedRows: false,
+      showRowHover: false,
+      selectable: true,
+      multiSelectable: false,
+      enableSelectAll: false,
+      deselectOnClickaway: true,
+      height: '300px',
+    };
+
     var rows = [];
 
     var hidden = {
@@ -52,6 +70,7 @@ module.exports = React.createClass({
     }
     // selectedEvent as object
     var s = ({});
+
     for(var e=0;e<this.props.events.length;e++)if(this.props.events[e].id == this.props.selectedEvent){s = this.props.events[e]}
 
     for (var i=0; i < this.props.users.length; i++) {
@@ -81,7 +100,7 @@ module.exports = React.createClass({
               className="profile-checkboxes"
             >
               <Checkbox label="Team Leader" ref={"team_lead_checkbox-"+this.props.users[i].id} key={this.props.id} defaultChecked={is_team_lead} className={i} onCheck={this.handleSubmit.bind(this, this.props.users[i].id, 'team_lead')} />
-              <Checkbox label="Staff" ref={"staff_checkbox-"+this.props.users[i].id} key={this.props.id} defaultChecked={is_staff} className={this.props.first_name} onCheck={this.handleSubmit.bind(this, this.props.users[i].id), 'staff'} />
+              <Checkbox label="Staff" ref={"staff_checkbox-"+this.props.users[i].id} key={this.props.id} defaultChecked={is_staff} className={this.props.first_name} onCheck={this.handleSubmit.bind(this, this.props.users[i].id, 'staff')} />
             </div>
 
             <CardMedia key={this.props.id} expandable={true} overlay={<CardTitle title={this.props.users[i].first_name} subtitle="subtitle"/>}>
@@ -94,6 +113,77 @@ module.exports = React.createClass({
         </div>
       )
     }
-    return <div key={1}>{rows}</div>;
+    return <div key={1}>    <Table
+      height={this.state.height}
+      fixedHeader={this.state.fixedHeader}
+      fixedFooter={this.state.fixedFooter}
+      selectable={this.state.selectable}
+      multiSelectable={this.state.multiSelectable}
+      onRowSelection={this._onRowSelection}>
+      <TableHeader enableSelectAll={this.state.enableSelectAll}>
+        <TableRow>
+          <TableHeaderColumn colSpan="3" tooltip='Super Header' style={{textAlign: 'center'}}>
+            Super Header
+          </TableHeaderColumn>
+        </TableRow>
+        <TableRow>
+          <TableHeaderColumn tooltip='The ID'>ID</TableHeaderColumn>
+          <TableHeaderColumn tooltip='The Name'>Name</TableHeaderColumn>
+          <TableHeaderColumn tooltip='The Status'>Status</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody
+        deselectOnClickaway={this.state.deselectOnClickaway}
+        showRowHover={this.state.showRowHover}
+        stripedRows={this.state.stripedRows}>
+      <TableRow selected={true}>
+          <TableRowColumn>1</TableRowColumn>
+          <TableRowColumn>John Smith</TableRowColumn>
+          <TableRowColumn>Employed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>2</TableRowColumn>
+          <TableRowColumn>Randal White</TableRowColumn>
+          <TableRowColumn>Unemployed</TableRowColumn>
+        </TableRow>
+        <TableRow selected={true}>
+          <TableRowColumn>3</TableRowColumn>
+          <TableRowColumn>Stephanie Sanders</TableRowColumn>
+          <TableRowColumn>Employed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>4</TableRowColumn>
+          <TableRowColumn>Steve Brown</TableRowColumn>
+          <TableRowColumn>Employed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>5</TableRowColumn>
+          <TableRowColumn>Joyce Whitten</TableRowColumn>
+          <TableRowColumn>Employed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>6</TableRowColumn>
+          <TableRowColumn>Samuel Roberts</TableRowColumn>
+          <TableRowColumn>Unemployed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>7</TableRowColumn>
+          <TableRowColumn>Adam Moore</TableRowColumn>
+          <TableRowColumn>Employed</TableRowColumn>
+        </TableRow>
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableRowColumn>ID</TableRowColumn>
+          <TableRowColumn>Name</TableRowColumn>
+          <TableRowColumn>Status</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn colSpan="3" style={{textAlign: 'center'}}>
+            Super Footer
+          </TableRowColumn>
+        </TableRow>
+      </TableFooter>
+      </Table>{rows}</div>;
   }
 });
